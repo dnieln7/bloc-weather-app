6eaada9369e01dda24a664edbc96946bd704f7ea
dnieln7 <dnieln7@gmail.com>
2022-10-10 18:54:43 -0500

    Added POSITION_STACK_API_URL to env

--- lib/env/app_env.dart
+++ lib/env/app_env.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppEnv {
+  final String positionStackApiURL = dotenv.get('POSITION_STACK_API_URL');
   final String positionStackApiKey = dotenv.get('POSITION_STACK_API_KEY');
 }
